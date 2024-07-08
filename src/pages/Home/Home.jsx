6202a85fb65a051d78d68ad12c53eb7e465ae966
RuslanStadnichenko <imanpepsi77@gmail.com>
2024-07-08 16:00:36 +0300

    dz8

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
-import { FaHouse } from 'react-icons/fa6';
+import { FcContacts } from 'react-icons/fc';
 import s from './Home.module.css';
 
 const Home = () => {
   return (
     <div className={s.div}>
-      <FaHouse className={s.icon} />
+      <FcContacts className={s.icon} />
       <h3 className={s.title}>Welcome to Phone book</h3>
     </div>
   );
